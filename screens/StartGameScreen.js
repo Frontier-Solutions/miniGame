d1472559df1986e92d2b6491db6c6d56ba257ef8
Rick <ricardo.henriques@accedo.tv>
2023-06-12 15:48:18 +0100

    shadows

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,13 +1,30 @@
-import { TextInput, View, Pressable } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
-
-      {/* BUTTONS */}
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+
+    elevation: 4, //shadows (androind only)
+
+    shadowColor: "black", //shadow (iOS)
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+  },
+});
